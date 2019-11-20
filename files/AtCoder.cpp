d8bfeb8ee0a2eed97fbd880ebd89c77c5ac98c9e
GODofMEGANE <s18195@tokyo.kosen-ac.jp>
2019-11-21 00:00:36 +0900

    [fix]updata

--- files/AtCoder.cpp
+++ files/AtCoder.cpp
@@ -1,36 +1,58 @@
-#include<bits/stdc++.h>
-#include <unistd.h>
-#include <random>
+#include <bits/stdc++.h>
+#include <time.h>
 using namespace std;
 
 typedef long long int LL;
 typedef long long int LLint;
-typedef pair<int,int> intpair;
-typedef pair<bool,bool> boolpair;
-typedef pair<string,string> strpair;
-typedef pair<LL,LL> LLpair;
-typedef pair<double,double> doublepair;
-typedef pair<float,float> floatpair;
-typedef vector<int> intvector;
-typedef vector<bool> boolvector;
-typedef vector<string> strvector;
-typedef vector<LL> LLvector;
-typedef vector<double> doublevector;
-typedef vector<float> floatvector;
-#define ED return 0;
-#define TEST cout << "OK" << endl;
+
+//Faster Program
+#define endl "\n"
+
+//Faster Coding
+#define ED return
+#define GET(a) int a;cin >> a
+#define SGET(a) string a;cin >> a
+#define CGET(a) char a;cin >> a
+#define PRINT(a) cout << a << endl
+#define ANS(a,b) int a;cin >> a;cout << b << endl
+#define SORT(vec) sort(vec.begin(),vec.end())
+#define REV(vec) reverse(vec.begin(),vec.end())
+
+//As Function
 #define UP(a,b) ((a+(b-1))/b)
-#define SORT(vec) sort(vec.begin(),vec.end());
-#define DOUBLECHANGE(count) cout << setprecision(count);
-#define REV(vec) reverse(vec.begin(),vec.end());
 #define ipow(x,y) LL(pow(x,y))
-#define INF 999999999
-const long long mod = 1000000007;
 
-int main() {
-	// random_device rnd;
-	// mt19937 mt(rnd());
-    cin.tie(0);
+//Debug
+#define TEST cout << "OK" << endl
+
+const double PI = M_PI;
+const long long INF = int(10e9-1);
+const long long MOD = int(10e9+7);
+
+//Settings
+const bool DO_BENCHMARK = false;
+const int DOUBLE_ACCURACY = 15;
+
+int main(){
+    cin.tie(nullptr);
     ios::sync_with_stdio(false);
-
+    cout << fixed << setprecision(DOUBLE_ACCURACY);
+    void solve();
+    if(DO_BENCHMARK){
+        clock_t start = clock();
+        solve();
+        clock_t end = clock();
+        const double time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
+        cout << endl << time << "[ms]" << endl;
+    }
+    else{
+        solve();
+    }
 }
+
+void solve(){
+    int N;
+    cin >> N;
+    cout << N << endl;
+}
+
