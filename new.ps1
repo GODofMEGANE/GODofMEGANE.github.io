c2adfd8a34c5a0155f0a69bcb8120bbcc740f065
GODofMEGANE <s18195@tokyo.kosen-ac.jp>
2022-04-10 20:35:24 +0900

    [add] article

--- new.ps1
+++ new.ps1
@@ -1,7 +1,7 @@
 Param([parameter(mandatory=$true)][String]$slug)
 $time = Get-Date -UFormat "%Y%m%d-%H%M%S"
 hugo new posts/$($slug)_$($time).md
-$file_contents = Get-Content ./content/posts/$($slug)_$($time).md -Encoding utf8 | % { $_ -creplace "$($slug)_$($time)", "$($slug)"}
+$file_contents = Get-Content ./content/posts/$($slug)_$($time).md -Encoding utf8 | % { $_ -creplace "title: ""$($slug)_$($time)""", "title: ""$($slug)"""}
 Write-Output $file_contents | Out-File ./content/posts/$($slug)_$($time).md -Encoding UTF8
 New-Item ./static/images/$($slug)_$($time) -ItemType Directory
 Start-Process -FilePath ./content/posts/$($slug)_$($time).md
